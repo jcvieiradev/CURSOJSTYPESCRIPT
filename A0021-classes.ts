dcf459dc325d2ce6c1913134ffc88761949ea795
Julio Cesar Vieira <121468467+jcvieiradev@users.noreply.github.com>
2025-02-04 16:14:28 +0000

    classe

---
+++ A0021-classes.ts
@@ -0,0 +1,11 @@
+export class Empresa {
+  public readonly nome: string;
+
+  constructor(nome: string) {
+    this.nome = nome;
+  }
+}
+
+const empresa1 = new Empresa("Udemy");
+
+console.log(empresa1);
